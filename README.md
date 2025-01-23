0a703bce51f30bee1783f53bf3efbf7522884e26
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-01-23 21:08:02 +0100

    feat: add a small Rust cli to help debugging (#1)
    * feat: add a small Rust cli to help debugging
    
    * feat: handle ProofMode
    
    * feat: check the .bin files has been replaced
    
    * docs: update README
    
    * docs: add on try CPU

--- README.md
+++ README.md
@@ -16,17 +16,24 @@ If not, please follow these steps:
  
 ## Go through these commands
 
-SP1 can be used in 3 modes: CPU, GPU and on the [Prover Network](https://docs.succinct.xyz/docs/generating-proofs/prover-network). Knowing which mode you are using,
-and if it works in another mode will help us debug your issue.
- 
-* If you have access to the Prover Network,
-  try [running your progam](https://docs.succinct.xyz/docs/generating-proofs/prover-network/usage) on it and see if it works. 
-* If you have a [supported GPU](https://docs.succinct.xyz/docs/generating-proofs/hardware-acceleration/cuda),
-  try running your program by prepending `SP1_PROVER=gpu` to you command to test that proving on your GPU works.
-* If you issue is either when using your GPU or the Prover Network,
-  try running your program by prepending `SP1_PROVER=cpu` to you command to test that proving on your CPU works.
+SP1 can be used in several different modes: Mock, CPU, GPU and the Prover Network. To help debug the source of the issue and determine if it's specific to a prover mode, please follow the steps below (moving onto the next stage if the previous one succeeds). Let us know which stage your proof failed in.
 
-And then report the command you ran and their output in the section below.
+* **Mock mode**  
+  Run `cargo run --release -- mock`. If this fails, your error is within the execution of the program (and is hardware-agnostic).
+* **Prover Network**  
+  If you are generating a proof in CPU/GPU mode and are seeing failures, you should attempt generating a proof on the Prover Network, by running `cargo run --release -- network`. If [running your progam](https://docs.succinct.xyz/docs/generating-proofs/prover-network/usage) on the Prover Network works, the issue is likely due to an issue on your local setup. If it fails, this indicates something is wrong with the Prover Network when generating proofs.  
+  
+  > [!NOTE]
+  > To test on the Prover Network, you need to have an API key and set it to the `NETWORK_PRIVATE_KEY` environment variable
+* **GPU mode**  
+  If you have a [supported GPU](https://docs.succinct.xyz/docs/generating-proofs/hardware-acceleration/cuda), Run `cargo run --release -- gpu` and see if it works.
+* **CPU mode**  
+  If testing on a GPU fails, Try on the CPU by running  `cargo run --release -- cpu` and see if it works.
+
+
+If any of the above fail, report the command you ran and their output in the section below.
+
+If all of the above succeed, the issue is likely due to the setup of your program not supplying valid input to the program (as you've now ran a stdin & program ELF that succeeds).
 
 ## Describe command for reproducible issue
  
---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::{fs::File, io::Read};
+
+use anyhow::{bail, Result};
+use clap::{Parser, ValueEnum};
+use hex_literal::hex;
+use sha2::{Digest, Sha256};
+use sp1_sdk::{Prover, ProverMode, SP1ProofMode, SP1Stdin};
+use universal_prover::UniversalProver;
+
+mod universal_prover;
+
+const FIBONACCI_PROGRAM_HASH: [u8; 32] =
+    hex!("45d1d3889583ac22808eb5be7316fdeb2a936a3da1ca4931d462e4706833fc2e");
+
+const FIBONACCI_STDIN_HASH: [u8; 32] =
+    hex!("47ff30c843f47b09affa3f52f5b0035959540cc9d523c718577bb5c29933e726");
+
+/// The arguments for the command.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The mode to run the prover in.
+    mode: ProverMode,
+
+    /// The proof mode to use.
+    #[arg(long, value_enum, default_value_t = ProofMode::Compressed)]
+    proof_mode: ProofMode,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum ProofMode {
+    Core,
+    Compressed,
+    Plonk,
+    Groth16,
+}
+
+impl From<ProofMode> for SP1ProofMode {
+    fn from(mode: ProofMode) -> Self {
+        match mode {
+            ProofMode::Core => SP1ProofMode::Core,
+            ProofMode::Compressed => SP1ProofMode::Compressed,
+            ProofMode::Plonk => SP1ProofMode::Plonk,
+            ProofMode::Groth16 => SP1ProofMode::Groth16,
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    // Parse the command line arguments.
+    let args = Args::parse();
+    let client = UniversalProver::new(&args.mode);
+    let program = read_program()?;
+    let stdin = read_stdin()?;
+
+    // Setup the program for proving.
+    let (pk, vk) = client.setup(&program);
+
+    let proof = client
+        .prove(&pk, &stdin, args.proof_mode.into())
+        .expect("failed to generate proof");
+    println!("Successfully generated proof!");
+
+    // Verify the proof.
+    client.verify(&proof, &vk).expect("failed to verify proof");
+    println!("Successfully verified proof!");
+
+    Ok(())
+}
+
+fn read_program() -> Result<Vec<u8>> {
+    let mut file = File::open("program.bin")?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    let mut hasher = Sha256::new();
+    hasher.update(&buffer);
+    let hash = hasher.finalize();
+
+    if hash[..] == FIBONACCI_PROGRAM_HASH {
+        bail!("Please replace the program.bin file with yours.");
+    }
+
+    Ok(buffer)
+}
+
+fn read_stdin() -> Result<SP1Stdin> {
+    let mut file = File::open("stdin.bin")?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    let mut hasher = Sha256::new();
+    hasher.update(&buffer);
+    let hash = hasher.finalize();
+
+    if hash[..] == FIBONACCI_STDIN_HASH {
+        bail!("Please replace the sdtin.bin file with yours.");
+    }
+
+    let stdin = bincode::deserialize(&buffer)?;
+
+    Ok(stdin)
+}
